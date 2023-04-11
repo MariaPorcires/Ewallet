7e6f4c3c1797b35aa6d677e2fe268d9095861e1d
hellnomary <maria.porcires@outlook.com>
2023-04-11 15:32:07 +0200

    form

--- E-wallet/src/components/CardForm/CardForm.jsx
+++ E-wallet/src/components/CardForm/CardForm.jsx
@@ -2,28 +2,43 @@ import './CardForm.scss'
 import { useState } from 'react'
 
 function CardForm() {
-    const [form, setForm] = useState()
+    const [nr, setNr] = useState();
+    const [name, setName] = useState();
+    const [date, setDate] = useState();
+    const [ccv, setCcv] = useState();
+    const [vendor, setVendor] = useState();
+
+    function handleClick() {
+        const showForm = {nr, name, date, ccv, vendor}
+        console.log(showForm);
+
+    }
 
     
-
     return(
         <section>
             <label htmlFor='cardnumber'>Card Number</label>
-            <input type='text' id='cardnumber' name='cardnumber' onChange={ (event) => { setForm(event.target.value)} }></input>
+            <input type='text' id='cardnumber' name='cardnumber' onChange={ (event) => { setNr(event.target.value)} } required />
             <label htmlFor='cardname'>Cardholder Name</label>
-            <input type='text' id='cardname' name='cardname' value='FIRSTNAME LASTNAME'></input>
-            <input type='text'></input>
-            <input type='text'></input>
+            <input type='text' id='cardname' name='cardname' onChange={ (event) => { setName(event.target.value)} } required />
+            <label htmlFor='date'>Valid thru</label>
+            <input type='text' id='date' name='date' onChange={ (event) => { setDate(event.target.value)} } required />
+            <label htmlFor='ccv'>CCV</label>
+            <input type='text' id='ccv' name='ccv' onChange={ (event) => { setCcv(event.target.value)} } required />
             <label htmlFor='vendor'>Vendor</label>
-                <select name='vendor' id='vendor'>
+                <select name='vendor' id='vendor' onChange={ (event) => { setVendor(event.target.value)} }>
                 <option value=''></option>
                     <option value='bitcoin'>BITCOIN INC</option>
                     <option value='ninja'>NINJA BANK</option>
                     <option value='block'>BLOCK CHAIN INC</option>
                     <option value='evil'>EVIL CORP</option>
                 </select>
+
+                <button onClick={ handleClick }>ADD CARD</button>
+
         </section>
     )
 }
+
 
 export default CardForm
