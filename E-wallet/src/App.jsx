305139400d6ffe77593f2328135ff21bf27008ca
hellnomary <maria.porcires@outlook.com>
2023-04-13 09:21:46 +0200

    router

--- E-wallet/src/App.jsx
+++ E-wallet/src/App.jsx
@@ -1,13 +1,27 @@
 import './App.scss'
-//import Home from './views/Home'
+import Home from './views/Home'
 import AddCart from './views/AddCart'
+//import CardForm from './components/CardForm/CardForm'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom' 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "addCart",
+    element: <AddCart />
+  }
+])
 
 function App() {
   
 
   return (
     <div className="App">
-      <AddCart />
+      <RouterProvider router={ router } />
+      
     </div>
   )
 }
