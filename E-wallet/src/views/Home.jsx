34d01f4c6eee1a76ab751f44d9fb625d3ed69007
hellnomary <maria.porcires@outlook.com>
2023-04-15 18:43:34 +0200

    update

--- E-wallet/src/views/Home.jsx
+++ E-wallet/src/views/Home.jsx
@@ -2,10 +2,16 @@ import Header from "../components/Header/Header";
 import './Home.scss'
 import { useSelector } from "react-redux";
 import CardStack from "../components/CardStack/CardStack";
+import { useNavigate } from "react-router-dom";
 
 
 function Home() {
-   <></>
+    const navigate = useNavigate()
+   
+
+    function handleClick() {
+        navigate("/AddCard")
+    }
     
     
 
@@ -15,7 +21,7 @@ function Home() {
             
             <section className="home__cardStack">
                 <CardStack />
-                <button className="home__button">ADD A NEW CARD</button>
+                <button className="home__button" onClick={ handleClick }>ADD A NEW CARD</button>
             </section>
         </section>
         
