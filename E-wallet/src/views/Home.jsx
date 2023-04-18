2c182844f5bb71665e80e45f531d1b2cece6e67c
hellnomary <maria.porcires@outlook.com>
2023-04-18 15:18:27 +0200

    active card

--- E-wallet/src/views/Home.jsx
+++ E-wallet/src/views/Home.jsx
@@ -25,15 +25,14 @@ function Home() {
                 <p className="add-card__type">ACTIVE CARD</p>
                 
 
-                <section className='card__img'style={{background: `${activeCard.vendor[2] }` }}>
-                    <img className='card__chip' src={ activeCard.vendor[4]} />
-                    
-                    <h2 className='card__nr' style={{color: `${activeCard.vendor[3] }` }}>{ activeCard.nr }</h2>
+                <section className='home__img' style={{...activeCard?.vendor ? {background: `${activeCard.vendor[2] }`} : '' }}>
+                    <img className='card__chip' src={ activeCard?.vendor ? activeCard.vendor[4] : '' } />
+                    <h2 className='card__nr' style={{ ...activeCard?.vendor ? {color: `${activeCard.vendor[3] }` } : '' }}>{ activeCard?.vendor ? activeCard.nr : '' }</h2>
                     <p className='card__name'>CARDHOLDER NAME</p>
-                    <p className='card__font' style={{color: `${activeCard.vendor[3] }` }}>{ activeCard.name }</p>
-                    <img className='card__logo' src={ activeCard.vendor[1] } />
+                    <p className='card__font' style={{...activeCard?.vendor ? {color: `${activeCard.vendor[3] }` } : '' }}>{ activeCard?.vendor ? activeCard.name : '' }</p>
+                    <img className='card__logo' src={ activeCard?.vendor? activeCard.vendor[1] : '' } />
                     <p className='card__date'>VALID THRU</p>
-                    <p style={{color: `${activeCard.vendor[3] }` }}>{ activeCard.date }</p>
+                    <p style={{...activeCard?.vendor ? {color: `${activeCard.vendor[3] }` } : '' }}>{ activeCard?.vendor ? activeCard.date : '' }</p>
                 </section>
 
                 
