8ea70f20f389ae16f34bb1db206c22323f719684
hellnomary <maria.porcires@outlook.com>
2023-04-18 09:49:29 +0200

    props color

--- E-wallet/src/components/Card/Card.jsx
+++ E-wallet/src/components/Card/Card.jsx
@@ -7,13 +7,12 @@ function Card(props) {
 
     return(
       
-     <section className='card'>
-            <h2>{ props.nr }</h2>
-            <p>{ props.name }</p>
-            <p>{ props.date }</p>
+     <section className='card' style={{background: `${props.vendor[2] }` }}>
+            <img src={ props.vendor[4]} />
+            <h2 style={{color: `${props.vendor[3] }` }}>{ props.nr }</h2>
+            <p style={{color: `${props.vendor[3] }` }}>{ props.name }</p>
+            <p style={{color: `${props.vendor[3] }` }}>{ props.date }</p>
             <img src={ props.vendor[1] } />
-          
-
            
         </section>
     )
